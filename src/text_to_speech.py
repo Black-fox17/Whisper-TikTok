b1f8a542414ba528e9b0befab050eea56231d5ed
Black-fox17 <93570850+Black-fox17@users.noreply.github.com>
2023-12-30 18:35:48 +0100

    Update text_to_speech.py
    Using the endpoint and the key.
    I made use of python requests library to make an http request to the endpoint with the text you want to convert to speech.

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,8 +1,39 @@
-import edge_tts
 
+import requests
+import base64
 
 async def tts(final_text: str, voice: str = "en-US-ChristopherNeural", stdout: bool = False, outfile: str = "tts.mp3", args=None) -> bool:
-    communicate = edge_tts.Communicate(final_text, voice)
-    if not stdout:
-        await communicate.save(outfile)
-    return True
+    endpoint = 'YOUR_ENDPOINT'
+    api_key = 'YOUR_API_KEY'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}',
+    }
+
+    data = {
+        'text': final_text,
+        'voice': voice,
+    }
+
+    response = requests.post(endpoint, json=data, headers=headers)
+
+    if response.status_code == 200:
+        audio_data = response.content
+
+        if stdout:
+            # If stdout is True, print the audio data to the console
+            print(audio_data)
+        else:
+            # If stdout is False, save the audio data to the specified file
+            with open(outfile, 'wb') as audio_file:
+                audio_file.write(audio_data)
+
+        return True
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        return False
+
+# Example usage
+text_to_speak = "Hello, this is a test."
+await tts(text_to_speak)
